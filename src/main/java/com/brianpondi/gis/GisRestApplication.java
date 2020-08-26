b2f22f04386b8f0ceae50d7bc811b0fab3e1edf7
PondiB <pondibrian@gmail.com>
2020-08-26 23:58:01 +0300

    :globe_with_meridians: maven dependancies

---
+++ src/main/java/com/brianpondi/gis/GisRestApplication.java
@@ -0,0 +1,13 @@
+package com.brianpondi.gis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GisRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GisRestApplication.class, args);
+    }
+
+}
